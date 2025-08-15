71e3d65f8b859773c85aa853015588353f4b8d10
Lugauls <lugauls@gmail.com>
2025-08-15 13:38:03 -0400

    resolvendo download de curriculo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,8 +29,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public\Curriculo.pdf"
-                download={Curriculo_Lucas.pdf}
+                href=""
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Baixar Curriculo
