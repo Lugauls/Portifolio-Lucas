8159651aace68c27dd5327069633e7112d6381b3
Lugauls <lugauls@gmail.com>
2025-08-15 13:30:45 -0400

    resolvendo problemas com botão

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="@\assets\Curriculo.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Baixar Curriculo
