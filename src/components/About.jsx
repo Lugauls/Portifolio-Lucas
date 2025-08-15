9fc52de2501bb97fa90889db558e46bdcf315134
Lugauls <lugauls@gmail.com>
2025-08-15 14:39:12 -0400

    --allow-empty

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Curriculo.pdf
+                href="/Curriculo.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Baixar Curriculo
