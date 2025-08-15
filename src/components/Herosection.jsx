819b98821c5783f779be3f91e5301486ff81f544
Lugauls <lugauls@gmail.com>
2025-08-15 01:05:44 -0400

    first commit

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -23,11 +23,6 @@ export const HeroSection = () => {
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
             Desenvolvedor iniciante em C# e .NET, com conhecimentos em front-end (Tailwind CSS, React) e interesse em DevOps com Microsoft Azure.          </p>
 
-          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-            <a href="#projects" className="cosmic-button">
-              Veja meus projetos
-            </a>
-          </div>
         </div>
       </div>
 
