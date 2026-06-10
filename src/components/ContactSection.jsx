8e334d7a3b81ed25ec43976724ab838c0a58a272
lugauls <lucasborges046@gmail.com>
2026-06-10 13:28:31 -0400

    mudanças na contactSection

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -33,7 +33,7 @@ export const ContactSection = () => {
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Get In <span className="text-primary"> Touch</span>
+          Entre em <span className="text-primary"> Contato </span>
         </h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
@@ -60,20 +60,6 @@ export const ContactSection = () => {
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
                     lucasborges046@gmail.com
-                  </a>
-                </div>
-              </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Phone className="h-6 w-6 text-primary" />{" "}
-                </div>
-                <div>
-                  <h4 className="font-medium"> Telefone</h4>
-                  <a
-                    href="tel:+11234567890"
-                    className="text-muted-foreground hover:text-primary transition-colors"
-                  >
-                    +55 (92) 99512-3761
                   </a>
                 </div>
               </div>
