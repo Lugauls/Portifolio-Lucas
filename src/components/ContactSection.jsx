7c12cf71b9ab41b46ebae2106e4d45dc2cf5ba60
lugauls <lucasborges046@gmail.com>
2026-06-26 14:20:58 -0400

    correções em contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -19,6 +19,11 @@ export const ContactSection = () => {
 
   const handleSubmit =  async (e) => {
     e.preventDefault();
+
+  console.log("Service ID:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
+  console.log("Template ID:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+  console.log("Public Key:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
     setIsSubmitting(true);
 
     try {
